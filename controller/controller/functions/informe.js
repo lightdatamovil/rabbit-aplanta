581424c1a56974a2efed71ca0e562f4effbd9c09
Agustt123 <agustintracheskyoficial@gmail.com>
2025-03-19 16:17:20 -0300

    1mera optimizacion

--- controller/controller/functions/informe.js
+++ controller/controller/functions/informe.js
@@ -1,18 +1,19 @@
 import { executeQuery, getClientsByCompany, getDriversByCompany } from "../../../db.js";
-import { logCyan, logRed, logYellow } from "../../../src/funciones/logsCustom.js";
+import { logCyan, logRed } from "../../../src/funciones/logsCustom.js";
 
 export async function informe(dbConnection, companyId, clientId, userId, shipmentId) {
     const hoy = new Date().toISOString().split('T')[0];
 
     try {
         const queryIngresadosHoy = `
-            SELECT eh.estado 
+            SELECT eh.estado, COUNT(eh.estado) AS total
             FROM envios_historial AS eh
             JOIN envios AS e 
                 ON e.elim=0 AND e.superado=0 AND e.didCliente = ? AND e.did = eh.didEnvio
             WHERE eh.elim=0 AND eh.superado=0 
             AND (eh.autofecha BETWEEN ? AND ?) 
-            AND eh.estado IN (7, 0, 1);
+            AND eh.estado IN (7, 0, 1)
+            GROUP BY eh.estado;
         `;
 
         const resultIngresadosHoy = await executeQuery(dbConnection, queryIngresadosHoy, [clientId, `${hoy} 00:00:00`, `${hoy} 23:59:59`]);
@@ -22,9 +23,9 @@ export async function informe(dbConnection, companyId, clientId, userId, shipmen
 
         resultIngresadosHoy.forEach(row => {
             if (row.estado === 1) {
-                amountOfARetirarAndRetirados++;
+                amountOfARetirarAndRetirados += row.total;
             } else {
-                amountOfAPlanta++;
+                amountOfAPlanta += row.total;
             }
         });
 
@@ -33,17 +34,13 @@ export async function informe(dbConnection, companyId, clientId, userId, shipmen
             FROM envios_historial 
             WHERE elim=0
             AND quien = ?
-            and ( autofecha > ?
-            and autofecha < ?)
+            AND (autofecha BETWEEN ? AND ?)
             AND estado = 1;
         `;
-        const resultIngresadosHoyChofer = await executeQuery(dbConnection, queryIngresadosHoyChofer, [userId, `${hoy} 00:00:00`, `${hoy} 23:59:59`]);
+        
+        const ingresadosHoyChofer = (await executeQuery(dbConnection, queryIngresadosHoyChofer, [userId, `${hoy} 00:00:00`, `${hoy} 23:59:59`]))[0]?.total || 0;
 
-        const ingresadosHoyChofer = resultIngresadosHoyChofer[0]?.total || 0;
-
-        let choferasignado;
-        let zonaentrega;
-        let sucursal;
+        let choferasignado, zonaentrega, sucursal;
 
         if (shipmentId > 0) {
             const queryEnvios = `
@@ -59,40 +56,38 @@ export async function informe(dbConnection, companyId, clientId, userId, shipmen
 
             if (resultEnvios.length > 0) {
                 choferasignado = resultEnvios[0].choferAsignado || 'Sin asignar';
-                zonaentrega = resultEnvios[0].zona || "Sin informacion";
-                sucursal = resultEnvios[0].sucursal || "Sin informacion";
+                zonaentrega = resultEnvios[0].zona || "Sin información";
+                sucursal = resultEnvios[0].sucursal || "Sin información";
             }
         }
 
-        const companyClients = await getClientsByCompany(dbConnection, companyId);
-
-        const companyDrivers = await getDriversByCompany(dbConnection, companyId);
+        const [companyClients, companyDrivers] = await Promise.all([
+            getClientsByCompany(dbConnection, companyId),
+            getDriversByCompany(dbConnection, companyId)
+        ]);
 
         if (companyClients[clientId] === undefined) {
             throw new Error("Cliente no encontrado");
         }
         logCyan("El cliente fue encontrado");
 
-        let chofer;
-
-        if (companyDrivers[choferasignado] === undefined) {
-            chofer = "Sin informacion";
+        const chofer = companyDrivers[choferasignado]?.nombre || "Sin información";
+        if (!companyDrivers[choferasignado]) {
             logCyan("El chofer no fue encontrado");
         } else {
-            chofer = companyDrivers[choferasignado].nombre;
             logCyan("El chofer fue encontrado");
         }
 
         logCyan("Se generó el informe");
 
         return {
-            cliente: `${companyClients[clientId]?.nombre || 'Sin informacion'}`,
+            cliente: `${companyClients[clientId]?.nombre || 'Sin información'}`,
             aingresarhoy: amountOfAPlanta,
             ingresadoshot: amountOfARetirarAndRetirados,
             ingresadosahora: ingresadosHoyChofer,
-            chofer: chofer,
-            zonaentrega: zonaentrega,
-            sucursal: sucursal
+            chofer,
+            zonaentrega,
+            sucursal
         };
 
     } catch (error) {
